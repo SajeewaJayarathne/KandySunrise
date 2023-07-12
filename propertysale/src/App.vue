3b8dee6b8bad7e49d84eb313fdef7a2b67267ce9
sajeewajay <sajeewagj@gmail.com>
2023-07-12 17:34:12 +1000

    Creating the Basic Vue project

---
+++ propertysale/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="hello">
+    Hello world!
+    <span class="hello-child">
+      This is the child...
+    </span>
+  </div>
+</template>
+
+<style lang="sass">
+
+</style>
